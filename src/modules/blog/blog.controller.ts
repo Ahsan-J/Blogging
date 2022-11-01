114e1cc3690cd188943b7e818cc6611520ebd4c3
Ahsan-J <ahsan.ahmed99@ymail.com>
2022-11-01 17:51:59 +0500

    Blog like, unlike added on QueryBuilder

--- src/modules/blog/blog.controller.ts
+++ src/modules/blog/blog.controller.ts
@@ -5,7 +5,6 @@ import { PaginationMeta, PaginationQuery } from "src/helper-modules/common/commo
 import { CommonService } from "src/helper-modules/common/common.service";
 import { Sieve } from "src/helper/sieve.pipe";
 import { getStorage } from "src/helper/utility";
-import { FindOptionsWhere, IsNull } from "typeorm";
 import { AuthUser } from "../auth/auth.decorator";
 import { AuthGuard } from "../auth/auth.guard";
 import { User } from "../user/user.entity";
@@ -25,17 +24,15 @@ export class BlogController {
     ) { }
 
     @Get()
-    async getBlogs(@Query() query: PaginationQuery, @Query('filters', Sieve) filters: Array<FindOptionsWhere<Blog>>, @Query('sorts', Sieve) sorts): Promise<Array<Blog> | { meta: PaginationMeta }> {
+    async getBlogs(@Query() query: PaginationQuery, @Query('filters', Sieve) filters, @Query('sorts', Sieve) sorts): Promise<Array<Blog> | { meta: PaginationMeta }> {
         const page = parseInt(query.page || '1');
         const pageSize = parseInt(query.pageSize || '10');
+
         const [data, count] = await this.blogService.getBlogs({
-            skip: (page - 1) * pageSize,
-            take: pageSize,
-            where: {
-                deleted_at: IsNull(),
-                ...filters,
-            },
-            order: sorts,
+            page,
+            pageSize,
+            filters,
+            sorts 
         });
 
         const meta = this.commonService.generateMeta(count, page, pageSize);
@@ -66,12 +63,47 @@ export class BlogController {
     async deleteBlog(@Param("id") id: string, @AuthUser() user: User) {
         const blog = await this.blogService.getBlogById(id);
 
-        if(blog.author.id !== user.id && !this.commonService.checkValue(user.role, UserRole.Admin)) {
+        if (blog.author.id !== user.id && !this.commonService.checkValue(user.role, UserRole.Admin)) {
             throw new ForbiddenException("Unable to perform delete on other's blog");
         }
 
         this.blogService.deleteBlog(blog);
     }
 
+    @Post(':id/like')
+    @UseGuards(AuthGuard)
+    @ApiBearerAuth('AccessToken')
+    async likeBlog(@Param("id") id: string, @AuthUser() user: User) {
+        const blog = await this.blogService.getBlogById(id);
+        return this.blogService.likeBlog(user, blog);
+    }
 
+    @Post(':id/unlike')
+    @UseGuards(AuthGuard)
+    @ApiBearerAuth('AccessToken')
+    async unlikeBlog(@Param("id") id: string, @AuthUser() user: User) {
+        const blog = await this.blogService.getBlogById(id);
+        return this.blogService.unlikeBlog(user, blog);
+    }
+
+    @Get(":id/likes")
+    async getPostLikes(@Param("id") id: string, @Query() query: PaginationQuery, @Query('sorts', Sieve) sorts) {
+        const page = parseInt(query.page || '1');
+        const pageSize = parseInt(query.pageSize || '10');
+
+        const blog = await this.blogService.getBlogById(id);
+
+        const [data, count] = await this.blogService.getBlogLikes(blog.id, {
+            page,
+            pageSize,
+            sorts 
+        });
+
+        const meta = this.commonService.generateMeta(count, page, pageSize);
+
+        return {
+            ...data,
+            meta
+        }
+    }
 }
--- src/modules/blog/blog.service.ts
+++ src/modules/blog/blog.service.ts
@@ -1,43 +1,94 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { FindManyOptions, Repository } from "typeorm";
+import { Repository } from "typeorm";
 import { Blog } from "./blog.entity";
 import { CreateBlog } from './blog.dto';
 import { User } from "../user/user.entity";
 import { BlogStatus } from "./blog.enum";
 import moment from "moment";
+import { ListOptions } from "src/helper/model";
 
 @Injectable()
-export class BlogService{
+export class BlogService {
 
     constructor(
         @InjectRepository(Blog)
         private blogRepository: Repository<Blog>,
-      ) { }
+    ) { }
 
-    async getBlogs(options: FindManyOptions<Blog>):Promise<[Blog[], number]> {
-        return await this.blogRepository.findAndCount(options);
+    async getBlogs(options: ListOptions): Promise<[Blog[], number]> {
+
+        const blogsQuery = await this.blogRepository.createQueryBuilder('blog')
+            .where(options.filters)
+            .limit(options.pageSize)
+            .offset((options.page - 1) * options.pageSize)
+            .orderBy(options.sorts);
+
+        return await blogsQuery.getManyAndCount();
     }
 
     async createBlog(newBlog: CreateBlog, author: User, banner: Express.Multer.File): Promise<Blog> {
-        return await this.blogRepository.save(({
+        const blog = await this.blogRepository.create({
             author,
             title: newBlog.title,
             content: newBlog.content,
             description: newBlog.description,
             status: BlogStatus.Published,
-        }))
+        });
+        return await this.blogRepository.save(blog);
     }
 
     async getBlogById(id: string): Promise<Blog> {
-        return await this.blogRepository.findOne({
-            where: {id},
-            relations: ['author', 'likes', 'unlikes', 'comments'],
-        });
+        return await this.blogRepository.createQueryBuilder('blog')
+            .where("blog.id=:id", { id })
+            .leftJoin('blog.likes', 'user.like_blogs')
+            .loadRelationCountAndMap('blog.likes_count', 'blog.likes')
+            .leftJoin('blog.comments', 'comment.blog')
+            .loadRelationCountAndMap('blog.comments_count', 'blog.comments')
+            .leftJoinAndSelect("blog.author", "user")
+            .printSql()
+            .getOne()
+    }
+
+    async toggleLike(user: User, blog: Blog) {
+        const queryBlog = await this.blogRepository.createQueryBuilder("blog")
+            .where("blog.id = :id", { id: blog.id })
+            .innerJoinAndSelect("blog.likes", "user", "user.id = :userId", { userId: user.id })
+            .getOne()
+
+        if (queryBlog && queryBlog.likes.some(u => u.id == user.id)) {
+            return await this.blogRepository.createQueryBuilder().relation("likes").of(blog).remove(user)
+        } else {
+            return await this.blogRepository.createQueryBuilder().relation("likes").of(blog).add(user)
+        }
+    }
+
+    async likeBlog(user: User, blog: Blog) {
+        const queryBlog = await this.blogRepository.createQueryBuilder("blog")
+            .where("blog.id = :id", { id: blog.id })
+            .innerJoinAndSelect("blog.likes", "user", "user.id = :userId", { userId: user.id })
+            .getOne()
+        if (!queryBlog) {
+            return await this.blogRepository.createQueryBuilder().relation("likes").of(blog).add(user)
+        }
+    }
+
+    async unlikeBlog(user: User, blog: Blog) {
+        return await this.blogRepository.createQueryBuilder().relation("likes").of(blog).remove(user)
     }
 
     async deleteBlog(blog: Blog): Promise<Blog> {
         blog.deleted_at = moment().toISOString();
         return this.blogRepository.save(blog);
+    }
+
+    async getBlogLikes(blogId: Blog['id'], options: ListOptions): Promise<[any[], number]> {
+        
+        const [blog, count] = await this.blogRepository.createQueryBuilder("blog")
+            .leftJoinAndSelect("blog.likes", "user")
+            .where("blog.id = :id", {id: blogId})
+            .getManyAndCount();
+
+        return [blog, count]; 
     }
 }
